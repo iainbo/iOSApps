1e9f37fd8360e89b671e45b7a22ea1fa809dd7d7
iainbo <iainbo@users.noreply.github.com>
2016-10-13 19:18:44 +0100

    Initial Commit

---
+++ DemoCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoCalculator
+//
+//  Created by Iain Bogucki on 13/10/2016.
+//  Copyright © 2016 IBApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
