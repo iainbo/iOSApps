afde4fcb99434c6bafb7877d2ce5039892216033
iainbo <iainbo@users.noreply.github.com>
2016-10-15 14:32:19 +0100

    Added various buttons and code to perform the required calculations. Sorted view to fit to portrait and landscape oreientation.

--- DemoCalculator/CalculatorModel.swift
+++ DemoCalculator/CalculatorModel.swift
@@ -16,12 +16,54 @@ class CalculatorModel{
         accumulator = operand
     }
     
+    private var operations: Dictionary<String, Operation> = [
+        "π" : Operation.Constant(M_PI),
+        "e" : Operation.Constant(M_E),
+        "√" : Operation.UnaryOperation(sqrt),
+        "cos" : Operation.UnaryOperation(cos),
+        "×": Operation.BinaryOperation({$0 * $1}),
+        "÷": Operation.BinaryOperation({$0 / $1}),
+        "+": Operation.BinaryOperation({$0 + $1}),
+        "−": Operation.BinaryOperation({$0 - $1}),
+        "=": Operation.Equals
+    ]
+    
+    private enum Operation{
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+        
+    }
+    
     func performOperation(symbol: String){
-        switch symbol{
-        case "π": accumulator = M_PI
-        case "√": accumulator = sqrt(accumulator)
-        default: break
+        if let operation = operations[symbol]{
+            switch operation{
+            case .Constant(let value):
+                accumulator = value
+            case .UnaryOperation(let function):
+                accumulator = function(accumulator)
+            case .BinaryOperation(let function):
+                executingPendingBinaryOperation()
+                pending = pendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+            case .Equals:
+                executingPendingBinaryOperation()
+            }
         }
+    }
+    
+    private func executingPendingBinaryOperation(){
+        if pending != nil {
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+        }
+    }
+    
+    //Optional property identified by ?
+    private var pending: pendingBinaryOperationInfo?
+    
+    private struct pendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double) -> Double
+        var firstOperand: Double
     }
     
     var result: Double{
